c068e5aeeff941d3487a75c9d9a0b49f2c35375e
Even Stensberg <evenstensberg@gmail.com>
2021-04-05 21:34:20 +0200

    chore: wip

--- react-ink.js
+++ react-ink.js
@@ -1,5 +1,7 @@
+import { cyan, yellow, bold } from 'colorette';
 import React, { useState, useEffect, useRef } from 'react';
 import { render, measureElement, Box, Text, useStdin, useStdout, useFocusManager, useInput } from 'ink';
+import SelectInput from 'ink-select-input';
 
 
 const EXIT_KEY = 'q';
@@ -38,32 +40,8 @@ const interactiveConfig = [
     },
 ];
 
-const writeFilterConsole = (stats) => {
-    const latestCompilation = stats;
-    const data = [];
 
-    for (let i = 0; i < latestCompilation.chunks.length; i++) {
-        const name = latestCompilation.chunks[i].names[0];
-        const chunksArr = [];
-        for (let j = 0; j < latestCompilation.chunks[i].modules.length; j++) {
-            const size = latestCompilation.chunks[i].modules[j].size;
-            const path = latestCompilation.chunks[i].modules[j].name.replace('./', '');
-            const issuerPath = latestCompilation.chunks[i].modules[j].issuerPath;
-            chunksArr.push({ path, size, issuerPath });
-        }
-        data.push({ [name]: chunksArr });
-    }
-    console.clear();
-    data.forEach((chunk) => {
-        Object.keys(chunk).forEach((mod) => {
-            console.log(bold(cyan(mod)));
-            chunk[mod].forEach((sub) => {
-                console.log('> ', yellow(sub.path));
-            });
-        });
-    });
-    process.stdout.write(ansiEscapes.cursorTo(0, 1));
-};
+let webpackProc = spawn("webpack", ['--profile', '--json', 'stats.json']);
 
 const Counter = () => {
     const ref = useRef();
@@ -71,8 +49,11 @@ const Counter = () => {
     const { write } = useStdout();
     const { enableFocus } = useFocusManager();
     const [state, setState] = useState({
-        index: 0
-    })
+        modules: [],
+        compiling: true,
+        compiledModule: {}
+    });
+
     useEffect(() => {
         const { width, height } = measureElement(ref.current);
         // width = 100, height = 1
@@ -83,6 +64,58 @@ const Counter = () => {
         };
     }, []);
 
+
+    webpackProc.stdout.on('data', (data) => {
+        console.clear();
+        write(data.toString());
+        setState({
+            compiling: false,
+            modules: []
+        });
+    });
+
+    webpackProc.stderr.on('data', (data) => {
+        console.clear();
+        write(data.toString());
+        setState({
+            compiling: false,
+            modules: [],
+            compiledModule: null
+        });
+    });
+
+    const writeFilterConsole = (stats) => {
+        const latestCompilation = stats;
+        const data = [];
+
+        for (let i = 0; i < latestCompilation.chunks.length; i++) {
+            const name = latestCompilation.chunks[i].names[0];
+            const chunksArr = [];
+            for (let j = 0; j < latestCompilation.chunks[i].modules.length; j++) {
+                const size = latestCompilation.chunks[i].modules[j].size;
+                const path = latestCompilation.chunks[i].modules[j].name.replace('./', '');
+                const issuerPath = latestCompilation.chunks[i].modules[j].issuerPath;
+                chunksArr.push({ path, size, issuerPath });
+            }
+            data.push({ [name]: chunksArr });
+        }
+        console.clear();
+
+        setState({
+            compiling: false,
+            modules: data,
+            compiledModule: null
+        })
+    };
+
+    const handleSelect = item => {
+        setState({
+            compiledModule: item,
+            isCompiling: false,
+            modules: [],
+        })
+    };
+
     useInput((input, key) => {
         if (input === 'q') {
             // Exit program
@@ -91,42 +124,87 @@ const Counter = () => {
         }
         interactiveConfig.forEach(prop => {
             if (prop.key === input || key.return) {
-                let webpackProc;
-                if (key.return) {
-                    webpackProc = spawn("webpack", ['--profile', '--json', 'stats.json']);
-                    webpackProc.stdout.on('data', (data) => {
-                        console.clear();
-                        write(data.toString());
+                if (key.return && !state.modules.length) {
+                    setState({
+                        compiling: true,
+                        modules: [],
+                        compiledModule: null
                     });
-
-                    webpackProc.stderr.on('data', (data) => {
-                        console.clear();
-                        write(data.toString());
-                    });
+                    write("Compiling...");
+                    webpackProc = spawn("webpack", []);
 
                 }
                 else if (input === ANALYZE_KEY) {
                     console.clear();
+                    setState({
+                        compiling: true,
+                        modules: [],
+                        compiledModule: null
+                    });
+                    write("Compiling...");
+                    webpackProc = spawn("webpack", []);
                     const bundleAnalyzer = require('webpack-bundle-analyzer');
                     const fs = require('fs');
                     const file = JSON.parse(fs.readFileSync('./stats.json', 'utf8'));
                     bundleAnalyzer.start(file);
                 }
                 else if (input === FILTER_KEY) {
-
+                    const fs = require('fs');
+                    const file = JSON.parse(fs.readFileSync('./stats.json', 'utf8'));
+                    console.clear();
+                    writeFilterConsole(file);
                 } else if (input === PAUSE_KEY) {
                     webpackProc.kill('SIGSTOP');
+                    setState({
+                        compiling: false,
+                        modules: [],
+                        compiledModule: null
+                    });
+                    console.clear();
+                    write("Paused!")
                 } else if (input === CONTINUE_KEY) {
                     webpackProc.kill('SIGCONT');
+                    setState({
+                        compiling: true,
+                        modules: [],
+                        compiledModule: null
+                    });
+                    write("Continuing...")
                 }
             }
         })
     });
+    if(state.compiledModule && (Object.keys(state.compiledModule).length > 0)) {
+        return (<Box width={100}>
+            <Box flexDirection="column" ref={ref} marginLeft={2}>
+            <Text color="gray">Path: <Text color="#FF4500">{state.compiledModule.label.toString()}</Text></Text>
+            <Text color="gray">Size: <Text color="#FF4500">{state.compiledModule.data.size.toString()}</Text></Text>
+            <Text color="gray">Issuerpath: <Text color="#FF4500">{state.compiledModule.data.issuerPath ? state.compiledModule.data.issuerPath.toString() : 'null'}</Text></Text>
+            </Box>
+        </Box>);
+    }
+    if (state.modules && state.modules.length > 0) {
 
+        let map = [];
+        state.modules.forEach((chunk) => {
+            let data;
+            data = Object.keys(chunk).forEach((mod) => {
+                let path = chunk[mod].forEach((sub) => {
+                    const path = mod + ' > ' + sub.path;
+                    map.push({ value: path.toUpperCase(), label: path, data: {
+                        issuerPath: sub.issuerPath,
+                        size: sub.size,
+                    }  });
+                });
+            });
+        });
+        return <SelectInput items={map} onSelect={handleSelect} />;
+    }
     return (
         <Box width={100}>
             <Box flexDirection="column" ref={ref} marginLeft={2}>
                 <Text color="gray">Interactive Usage</Text>
+                <Text color="gray">Compiling: <Text color="#FF4500">{state.compiling ? state.compiling.toString() : 'false'}</Text></Text>
                 {interactiveConfig.map(prop => {
                     return (<Box key={prop.key}>
                         <Text color="gray" height={2} marginLeft={5} marginRight={2}>> Press </Text>
